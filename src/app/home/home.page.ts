0685f8e912c2db24980d3821340c100b2e6e2e99
karuraJeff4 <79208328+karuraJeff4@users.noreply.github.com>
2021-02-27 23:04:40 -0300

    Criação

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { throws } from 'assert';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public ToastController: ToastController) { 
+  }
+
+ async msgTopo() {
+    console.log("TOPO")
+    const toast = await this.ToastController.create({
+      message: "Mensagem exibida no Topo",
+      duration: 10000,
+      position: "top"
+    });
+    console.log(toast);
+    toast.present();
+  }
+ async msgCentro() {
+    console.log("Centro")
+    const toast = await this.ToastController.create({
+      message: "Mensagem exibida no Meio",
+      duration: 10000,
+      position: "middle"
+    });
+    console.log(toast);
+    toast.present();
+  }
+ async msgRodape() {
+    console.log("Rodape")
+    const toast = await this.ToastController.create({
+      message: "Mensagem exibida no Rodape",
+      duration: 10000,
+      position:"bottom"                       
+    });
+    console.log(toast);
+    toast.present();
+  }
 
 }
