2ec2d76264e71b64996c520f57fdf73e659cd7d5
karuraJeff4 <79208328+karuraJeff4@users.noreply.github.com>
2021-03-03 20:55:47 -0300

    adicionando escolhas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,9 +11,43 @@ export class HomePage {
 
   constructor(public ToastController: ToastController) { 
   }
+  users: any[] = [
+    {
+      id: 1,
+      first: 'Alice',
+      last: 'Smith',
+    },
+    {
+      id: 2,
+      first: 'Bob',
+      last: 'Davis',
+    },
+    {
+      id: 3,
+      first: 'Charlie',
+      last: 'Rosenburg',
+    }
+  ];
+  compareWithFn = (o1, o2) => {
+    return o1 && o2 ? o1.id === o2.id : o1 === o2;
+  };
+  compareWith = this.compareWithFn;
 
 async msgtoast(msg:string , duration:number, position: any){
   const toast = await this.ToastController.create({
+    header:"Informação importante",
+    buttons:[
+      {
+      side:"start",
+      icon:"car",
+      text :" Favoritos",
+      handler:() => {
+        console.log("Adicionando aos Favoritos - Igual o Exemplo");
+      }
+    }
+  ],
+    animated:false,
+    cssClass:"toastpersonalizado",
     message: msg,
     duration: duration,
     position: position
